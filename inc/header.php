263e459ffc1dfe5c0dfdc49e23ce6325d3343da9
Oli <33892974+Oli08@users.noreply.github.com>
2017-11-24 16:29:03 +0600

    Created Header Section as tariq draw the Diagram

---
+++ inc/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Employee Management System</title>
+	<link rel="SHORTCUT ICON" href="img/logo.png" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+</head>
+<body>
